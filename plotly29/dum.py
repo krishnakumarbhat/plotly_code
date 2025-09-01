8d406c3d998e8967584a51a178f518357ae7a738
krishnakumarbhat <79183768+krishnakumarbhat@users.noreply.github.com>
2025-09-01 21:27:01 +0530

    Pushed plotly29

---
+++ plotly29/dum.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+my_dict = {
+    '23': np.array([[1, 2], [3,5]][])
+}
+
+d2  = [0,1]
+
+dict2 = np.array([[1, 3],
+                   [5, 4]]) 
+
+my_dict['rows'] = []
+
+for row in dict2:
+    for j in d2:
+        if j = 0:
+            my_dict['rows'].append(row) 
+    
+my_dict['rows'] = np.array(my_dict['rows'])
+
+print(my_dict)
