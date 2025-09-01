73137f7d45fee5f8a15ad735d8aff6be43f14cd4
krishnakumarbhat <79183768+krishnakumarbhat@users.noreply.github.com>
2025-09-01 21:27:14 +0530

    Pushed plotly_40

---
+++ plotly_40/InteractivePlot/c_data_storage/regex_storage.py
@@ -0,0 +1,14 @@
+# Dictionary of signal patterns with their aliases
+SIGNAL_PATTERNS = {
+    'stream_hdr': ['stream_header', 'stream_hdr'],
+    # 'pow': ['pow', 'power'],
+    # 'phi': ['phi', 'phase'],
+    # 'ran': ['ran', 'range'],
+    # 'rcs': ['rcs', 'radar_cross_section'],
+    # 'rdd_idx': ['rdd_idx', 'rdd_index'],
+    # 'snr': ['snr', 'signal_to_noise_ratio'],
+    # 'f_bistaic': ['f_bistaic', 'frequency_bistaic'],
+    'vel': ['vel', 'velocity'],
+    'num_af_det': ['num_af_det', 'af_det'],
+    
+}
