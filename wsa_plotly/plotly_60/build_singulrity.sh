69385e1d70b9437d57a7c3bad77900a39708282b
Krishnakumar Bhat <Krishnakumar.Balachandra.Bhat@aptiv.com>
2025-11-21 15:37:24 +0530

    added org data

--- wsa_plotly/plotly_60/build_singulrity.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-
-# Build Docker image
-docker build -t resim-html-report .
-
-# Convert Docker image to Singularity
-singularity build resim-html-report.sif docker-daemon://resim-html-report:latest
