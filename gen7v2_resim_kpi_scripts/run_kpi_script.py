0e60f664814dc35155e98d5da0470ff2bebcbef0
Balachandra Bhat <Krishnakumar.balachandra.bhat@aptiv.com>
2026-02-26 00:02:07 +0530

    removed all unwanted

--- gen7v2_resim_kpi_scripts/run_kpi_script.py
+++
@@ -1,9 +0,0 @@
-
-import os
-import platform
-import subprocess
-
-if platform.system() == "Windows":
-   subprocess.run(["cmd", "/c", "run_kpi_script.bat"])
-else:
-   subprocess.run(["bash", "run_kpi_script.sh"])
